aaf5d2b0df2337c06d892193bc2a204f9e13f349
bala-amypo <bala@amypo.in>
2025-12-26 08:02:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/FinancialProfile.java
+++ src/main/java/com/example/demo/entity/FinancialProfile.java
@@ -1,8 +1,13 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+
 @Entity
 public class FinancialProfile {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
@@ -18,7 +23,69 @@ public class FinancialProfile {
 
     @PrePersist
     @PreUpdate
-    void updateTimestamp() {
+    protected void onUpdate() {
         lastUpdatedAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    // ===== GETTERS & SETTERS =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+ 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(Double monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public Double getMonthlyExpenses() {
+        return monthlyExpenses;
+    }
+
+    public void setMonthlyExpenses(Double monthlyExpenses) {
+        this.monthlyExpenses = monthlyExpenses;
+    }
+
+    public Double getExistingLoanEmi() {
+        return existingLoanEmi;
+    }
+
+    public void setExistingLoanEmi(Double existingLoanEmi) {
+        this.existingLoanEmi = existingLoanEmi;
+    }
+
+    public Integer getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(Integer creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public Double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public void setSavingsBalance(Double savingsBalance) {
+        this.savingsBalance = savingsBalance;
+    }
+
+    public Timestamp getLastUpdatedAt() {
+        return lastUpdatedAt;
     }
 }
